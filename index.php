cfcdb36e36099dac2d54c6e8080a0002bd36b7d1
Daniel <danisoria95@hotmail.com>
2019-11-08 10:17:10 +0100

    Versión 1.0 de mi tienda online jeje

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metagame</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <header>
+        <div id="logo">
+            <img src="imagenes/logo.png" alt="Metagame"/>
+        </div>
+        <div id="eslogan">
+            <p>Lo último en el mundo de los videojuegos</p>
+        </div>
+        <form id="login">
+            <div id="user">
+                <label>Usuario: </label>
+                <input type="text" id="usuario">
+            </div>
+            <div id="pass">
+                <label>Contraseña: </label>
+                <input type="password" id="contrasenia">
+            </div>
+        </form>
+    </header>
+    <main>
+        
+    </main>
+</body>
+</html>
